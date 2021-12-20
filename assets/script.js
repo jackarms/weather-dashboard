472ccb2b6e0e3bd86b9fac4bf3afd9ed1f82206b
jackarms <90922918+jackarms@users.noreply.github.com>
2021-12-19 22:45:48 -0600

    addition of javascript code

--- assets/script.js
+++ assets/script.js
@@ -1,28 +1,25 @@
-var citySearch = $("input").text();
-
-//console.log(citySearch);
 $(document).ready(function () {
   $("#searchBtn").click(function () {
     var citySearchText = $("input").text();
     var citySearchValue = $("input").val();
+    localStorage.setItem(citySearchText, citySearchValue);
 
     var getCityWeather = function () {
-      let city = localStorage.getItem();
+      let city = localStorage.getItem(citySearchText);
       console.log(city);
       let apiUrl =
         "https://api.openweathermap.org/data/2.5/weather?q=" +
         city +
-        "&appid=3727e6df52e0ca7f0482d054586ff709";
-      fetch(apiUrl);
+        "&appid=3727e6df52e0ca7f0482d054586ff709&units=imperial";
+      async function loadWeather() {
+        const response = await fetch(apiUrl);
+        const weather = await response.json();
+        theWeather = JSON.parse(weather);
+        temp = theWeather["temp"];
+        console.log(temp);
+      }
+      loadWeather();
     };
     getCityWeather();
   });
 });
-
-//var searchHistory = function (cityName) {
-//var returnValue = localStorage.getItem(citySearchValue);
-// $("#searcResults").after(returnValue);
-//};
-//searchHistory();
-
-//function fetchCall(value) {}
